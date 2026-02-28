e4dea06cfa46a97e3a40526a4aba1c72ba13890c
Кислицын Егор <kislitsyn1999@gmail.com>
2026-02-28 17:07:13 +0500

    fix: заменён clsx на утилиту cn, исправлен lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,11 +1,4 @@
-import path from 'path';
-
-const buildEslintCommand = filenames =>
-  `next lint --fix --file ${filenames
-    .map(f => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
-
 export default {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': ['eslint --max-warnings=0'],
   '*.{css,scss,json,md,html}': ['prettier --write'],
 };
