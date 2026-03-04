b79a3e32b84c8702b5ff7c26008bf6265968248d
Кислицын Егор <kislitsyn1999@gmail.com>
2026-03-04 17:28:32 +0500

    feat: добавленны домены аватаров Яндекс и ВК

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,18 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.yandex.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.userapi.com',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
